78366d02421e82007fe88965950949996385cf7f
arya892004 <Aryasinghkaushik@gmail.com>
2024-10-18 17:02:48 +0530

    Add files via upload

---
+++ oop1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Employee {
+    private:
+    int salary;
+    public:
+    void setsalary(int s) {
+        salary = s;
+    }
+    int getsalary() {
+        return(salary);
+    }
+};
+int main()
+{ 
+    employee obj;
+    obj.setsalary(50000);
+    cout << obj.getsalary()<<endl;
+    return 0;
+    }
